f3e25c78834338519afb39fdbc1bc8d9ee2fab5e
mahdi <71930380@students.liu.edu.lb>
2024-04-04 18:20:59 +0300

    get data of clicked post

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-setUpUI();
-getPostsReguest();
-let currentpage=1;
-let lastPage=1;
+
 // TODO[]fix ui code
 function setUpUI() {
   const token = localStorage.getItem("token");
+  const addBtn = document.getElementById("create-post-btn");
 
   if (token == null) {
-    document
-      .getElementById("create-post-btn")
-      .style.setProperty("display", "none", "important");
+    if (addBtn != null) {
+      addBtn.style.setProperty("display", "none", "important");
+    }
     document
       .getElementById("profile-image-nav")
       .style.setProperty("display", "none", "important");
@@ -22,9 +20,9 @@ function setUpUI() {
     //the user here is quest since there is no token
   } else {
     document.getElementById("loginbtnnav").style.display = "none";
-    document
-      .getElementById("create-post-btn")
-      .style.setProperty("display", "flex", "important");
+    if (addBtn != null) {
+      addBtn.style.setProperty("display", "flex", "important");
+    }
     document
       .getElementById("profile-image-nav")
       .style.setProperty("display", "flex", "important");
@@ -55,86 +53,7 @@ function fillnav() {
     userProfile.src = `${user.profile_image}`;
   }
 }
-function getPostsReguest(page=1) {
-  axios
-    .get(`https://tarmeezacademy.com/api/v1/posts?limit=1&page=${page}`)
-    .then(function (response) {
-      lastPage=response.data.meta.last_page;
-      // handle success
-      let postdata = response.data.data;
-      console.log(postdata);
-      fillPosts(postdata);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    });
-}
-function fillPosts(data) {
-  for (post of data) {
-    let postid = post.id;
-    let content = `<div class="card my-3 shadow-lg">
-        <div class="card-header">
-          <img
-            class="rounded-circle border border-2"
-            style="width: 40px"
-            height="40px"
-            src="${post.author.profile_image}"
-            alt=""
-          />
-          <span class="bold">${post.author.username}</span>
-        </div>
-        <div class="card-body">
-          <img
-            class="cover"
-            src="${post.image}"
-            alt=""
-            style="width: 100%"
-            height="350px"
-          />
-          <h6 class="mt-1">${post.created_at}</h6>
-          <h5>${post.title}</h5>
-          <p>
-          ${post.body}
-          </p>
-          <hr />
-          <div class="d-flex align-items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-chat-dots"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2"
-              />
-              <path
-                d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9 9 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.4 10.4 0 0 1-.524 2.318l-.003.011a11 11 0 0 1-.244.637c-.079.186.074.394.273.362a22 22 0 0 0 .693-.125m.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6-3.004 6-7 6a8 8 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a11 11 0 0 0 .398-2"
-              />
-            </svg>
-          
-            <span>
-            (${post.comments_count}) Comments
-            <span id="post-${postid}">
-            </span>
-            </span>
-          </div>
-        </div>
-        </div> `;
-    const currenttag = `post-${post.id}`;
-    let tagas = ["hello", "testu", "mahdi"];
-    document.getElementById("postscards").innerHTML += content;
-    document.getElementById(`${currenttag}`).innerHTML = ``;
-    for (tag of tagas) {
-      let content = ` <button class="btn btn-sm rounded-3" style="background-color:gray;color:white">
-        ${tag}
-        </button>`;
-      document.getElementById(`${currenttag}`).innerHTML += content;
-    }
-  }
-}
+
 //login function
 
 document.getElementById("loginbtn").addEventListener("click", () => {
@@ -236,44 +155,7 @@ function registerBtnClicked() {
       showAlert(msg, "danger");
     });
 }
-function createNewPostClicked() {
-  const body = document.getElementById("create-body-input").value;
-  const title = document.getElementById("create-title-input").value;
-  const image = document.getElementById("create-image-input").files[0];
-  let formData = new FormData();
-  formData.append("title", title);
-  formData.append("body", body);
-  formData.append("image", image);
-  const token = localStorage.getItem("token");
-  const url = "https://tarmeezacademy.com/api/v1/posts";
-  const header = {
-    "Content-Type": "multipart/form-data",
-    authorization: `Bearer ${token}`,
-  };
-  axios
-    .post(url, formData, {
-      headers: header,
-    })
-    .then((response) => {
-      console.log(response);
-      const modal = document.getElementById("add-post-modal");
-      const modelInstance = bootstrap.Modal.getInstance(modal);
-      modelInstance.hide();
-      showAlert("post add done", "success");
-      setTimeout(() => {
-        getPostsReguest();
-      }, 800);
-    })
-    .catch((error) => {
-      const modal = document.getElementById("add-post-modal");
-      const modelInstance = bootstrap.Modal.getInstance(modal);
-      modelInstance.hide();
-      console.log(error);
-      showAlert(`${error.response.data.message}`, "danger");
-      console.log(error);
-    });
-  //   close model
-}
+
 function getCurrentUser() {
   let user = null;
   const storageUser = localStorage.getItem("currentUser");
@@ -282,20 +164,5 @@ function getCurrentUser() {
   }
   return user;
 }
+
 //pagination and infinite scroll
-window.addEventListener("scroll",()=>{
-  const endOfPage=window.innerHeight+window.pageYOffset>=document.body.offsetHeight;
-  
-  if(endOfPage &&currentpage<=lastPage){
-    currentpage++;
-    console.log(currentpage)
-    getPostsReguest(currentpage)
-    console.log(currentpage,lastPage)
-    setTimeout((
-      
-    )=>{
-    
-    },1000)
-    
-  }  
-})
